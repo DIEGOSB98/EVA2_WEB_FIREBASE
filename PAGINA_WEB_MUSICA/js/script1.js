827394026626b588f9bb5b79053935c836c101fe
diegobinario <diegosbquimica@gmail.com>
2019-06-05 17:35:56 -0400

    En proceso acceso google...

--- PAGINA_WEB_MUSICA/js/script1.js
+++ PAGINA_WEB_MUSICA/js/script1.js
@@ -1,8 +1,2 @@
-firebase.initializeApp({
-    apiKey: 'AIzaSyBTGMQXY1qvq9Cj9i2cZdJBB0Iju8fQG6Q',
-    authDomain: 'eva2-e3377.firebaseapp.com',
-    projectId: 'eva2-e3377'
-  });
-  
-  // Initialize Cloud Firestore through Firebase
-  var db = firebase.firestore();
+
+
