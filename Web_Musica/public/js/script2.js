c634765c7ddffc438a18658d853747be772ac1de
diegobinario <diegosbquimica@gmail.com>
2019-06-10 14:25:54 -0400

    sobre nosotros modificado..

--- Web_Musica/public/js/script2.js
+++ Web_Musica/public/js/script2.js
@@ -1,15 +1,14 @@
 
-function validar_datos_administrador(){
+function validar_datos_administrador() {
 
-    var usuario_administrador=document.getElementById("usuario").value;
-    var contraseña_usuario=document.getElementById("contraseña").value;
+    var usuario_administrador = document.getElementById("usuario").value;
+    var contraseña_usuario = document.getElementById("contraseña").value;
 
     if(usuario_administrador=="" || contraseña_usuario==""){
-        alert("Llenar los campos es obligatorio.");
-    }else if(usuario_administrador=="Diego.Benaldo" && contraseña_usuario=="diego1998"){
-        alert("Datos Correctos.");
-        location.href='index.html';
-    }else{
+        alert("Por favor llene todos los campos.");
+    } else if (usuario_administrador == "Diego.Benaldo" && contraseña_usuario == "diego1998") {
+        location.href = 'menu-administrador.html';
+    } else {
         alert("Datos incorrectos !!!");
     }
 }
